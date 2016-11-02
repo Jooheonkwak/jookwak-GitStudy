32b937bcbf5a76cea2b94d70533b0727ca3c2297
Joo Heon Kwak <jooheon4@gmail.com>
2016-11-02 14:37:00 -0700

    Initial Project Version jookwak 11022016 GitStudy with Git Tutorial by derekbanas

---
+++ helloworld.c
@@ -0,0 +1,7 @@
+/* jookwak 09242016 first hello world in C program study 
+ * gcc -o testcprog HelloWorld.c ./testprog */
+#include <stdio.h>
+int main()
+{
+	printf ("Hello World!\n");
+}
---
+++ helloworld1.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/env perl 
+#===============================================================================
+#
+#         FILE: example.pl
+#
+#        USAGE: ./example.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: YOUR NAME (), 
+# ORGANIZATION: 
+#      VERSION: 1.0
+#      CREATED: 2016년 08월 19일 20시 21분 09초
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+use utf8;
+
+#myFunction():
+
+
+#===  FUNCTION  ================================================================
+#         NAME: myFunction
+#      PURPOSE: 
+#   PARAMETERS: ????
+#      RETURNS: ????
+#  DESCRIPTION: ????
+#       THROWS: no exceptions
+#     COMMENTS: none
+    # SEE ALSO: n/a
+#
+#    
+#    jookwak 08192015 tcomment study
+#    tcomment examples:
+#     highlight the above comment lines and gc
+#     to toggle a single line gcc
+#     
+#     jookwak 08192016 mru study
+#     :MRU
+#===============================================================================
+
+#sub myFunction {
+	#my	( $par1 )	= @_;
+	print "Hello World!\n";
+#	return ;
+#} ## --- end sub myFunction
+#---------------------------------------------------------------------
+#  Hello World
+#---------------------------------------------------------------------
